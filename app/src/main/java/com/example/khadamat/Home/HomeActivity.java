7531ff19d885e19aa4a004489b933c9c6c9fcf57
adchouaib <adraouichouaib44@gmail.com>
2021-01-12 22:36:14 +0100

    service activity

--- app/src/main/java/com/example/khadamat/Home/HomeActivity.java
+++ app/src/main/java/com/example/khadamat/Home/HomeActivity.java
@@ -2,9 +2,12 @@ package com.example.khadamat.Home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.khadamat.R;
+import com.example.khadamat.service.ServiceActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void Service(View view) {
+        Intent intent = new Intent(HomeActivity.this, ServiceActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/khadamat/service/ServiceActivity.java
@@ -0,0 +1,14 @@
+package com.example.khadamat.service;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.khadamat.R;
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.servicee);
+    }
+}
